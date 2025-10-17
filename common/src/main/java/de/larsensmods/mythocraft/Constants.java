b320788b432e18d304e35da117e034433c6d6bfc
Larsen Rektor <larsen@lrektor.de>
2025-10-18 00:55:48 +0200

    Initial commit

---
+++ common/src/main/java/de/larsensmods/mythocraft/Constants.java
@@ -0,0 +1,11 @@
+package de.larsensmods.mythocraft;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "mythocraft";
+    public static final String MOD_NAME = "Mythocraft";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
