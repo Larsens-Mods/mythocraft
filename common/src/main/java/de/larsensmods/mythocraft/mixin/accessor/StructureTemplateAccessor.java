00dc56580c0d3b67ac1abae602ddc828e936cad6
Larsen Rektor <larsen@lrektor.de>
2026-04-03 01:31:11 +0200

    Fixed access widener not properly working sometimes in production, tested for 1.21/1.21.1

---
+++ common/src/main/java/de/larsensmods/mythocraft/mixin/accessor/StructureTemplateAccessor.java
@@ -0,0 +1,18 @@
+package de.larsensmods.mythocraft.mixin.accessor;
+
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(StructureTemplate.class)
+public interface StructureTemplateAccessor {
+
+    @Accessor("palettes")
+    List<StructureTemplate.Palette> mythocraft$getPalettes();
+
+    @Accessor("entityInfoList")
+    List<StructureTemplate.StructureEntityInfo> mythocraft$getEntityInfoList();
+
+}
